321cdbe464f48c076a972bfb8f382d408a8f7815
shahjalal <rjshahjalal132@gmail.com>
2025-01-01 12:30:03 +0600

    display linkedin share button wich required data

--- inc/classes/class-share-in-linkedin.php
+++ inc/classes/class-share-in-linkedin.php
@@ -10,24 +10,26 @@ class Share_In_Linkedin {
     use Singleton;
     use Program_Logs;
 
+    public $user_urn;
+
     public function __construct() {
         $this->setup_hooks();
     }
 
     public function setup_hooks() {
         add_filter( 'the_content', [ $this, 'add_social_share_buttons' ] );
+
+        // set user urn
+        $this->user_urn = "JyqN44xm9_";
     }
 
     public function add_social_share_buttons( $content ) {
         if ( is_single() && in_the_loop() && is_main_query() ) {
             // HTML for the social buttons
-            $social_buttons = '
-            <div class="gli-social-share-buttons">
-                <a href="https://www.linkedin.com/shareArticle?mini=true&url=' . urlencode( get_permalink() ) . '" target="_blank" class="btn btn-linkedin">LinkedIn</a>
-                <a href="https://www.facebook.com/sharer/sharer.php?u=' . urlencode( get_permalink() ) . '" target="_blank" class="btn btn-facebook">Facebook</a>
-                <a href="https://twitter.com/intent/tweet?url=' . urlencode( get_permalink() ) . '" target="_blank" class="btn btn-twitter">Twitter</a>
-                <a href="https://api.whatsapp.com/send?text=' . urlencode( get_permalink() ) . '" target="_blank" class="btn btn-whatsapp">WhatsApp</a>
-            </div>';
+            $social_buttons = "
+            <div class='gli-social-share-buttons'>
+                <a href='JavaScript:void(0)' id='gli-share-linkedin' data-user-urn='" . $this->user_urn . "' data-post-url='" . get_permalink() . "' data-post-title='" . get_the_title() . "' class='btn btn-linkedin'>LinkedIn</a>
+            </div>";
 
             // Append the buttons to the post content
             return $content . $social_buttons;
